b104e6d24686d76adf105f7e4a1204f629bce0bb
Jason Morrison <jason.p.morrison@gmail.com>
2008-08-17 17:30:40 +0000

    cleaning up

--- pgp-enroll/app/controllers/admin/homes_controller.rb
+++ pgp-enroll/app/controllers/admin/homes_controller.rb
@@ -1,7 +1,4 @@
 class Admin::HomesController < Admin::AdminControllerBase
-  ADMIN_RESOURCES = %w(users content_areas exam_definitions)
-
   def index
-    @admin_resources = ADMIN_RESOURCES
   end
 end
