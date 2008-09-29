40a1ed0e74f813a13fbc6e2b531634322fc35f31
Jason Morrison <jason.p.morrison@gmail.com>
2008-09-29 04:13:09 +0000

    fixed typo in migration

--- pgp-enroll/db/migrate/20080907223203_move_content_area_id_from_exam_version_to_exam.rb
+++ pgp-enroll/db/migrate/20080907223203_move_content_area_id_from_exam_version_to_exam.rb
@@ -2,12 +2,12 @@ class MoveContentAreaIdFromExamVersionToExam < ActiveRecord::Migration
   def self.up
     add_column 'exams', 'content_area_id', 'integer'
     execute 'UPDATE exams SET content_area_id = (select exam_versions.content_area_id from exam_versions where exam_versions.exam_id = exams.id)'
-    remove_column 'exam_versions', 'content_area_id', 'integer'
+    remove_column 'exam_versions', 'content_area_id'
   end
 
   def self.down
     add_column 'exam_versions', 'content_area_id', 'integer'
     execute 'UPDATE exam_versions SET content_area_id = (select exams.content_area_id from exams where exam_versions.exam_id = exams.id)'
-    remove_column 'exams', 'content_area_id', 'integer'
+    remove_column 'exams', 'content_area_id'
   end
 end
