45583aa9af2659db8402bc336d54663c8e2188c7
Jason Morrison <jason.p.morrison@gmail.com>
2008-10-28 20:20:46 +0000

    touching migration

--- pgp-enroll/db/migrate/20081028195921_normalize_check_all_answers.rb
+++ pgp-enroll/db/migrate/20081028195921_normalize_check_all_answers.rb
@@ -1,31 +1,10 @@
-class Exam < ActiveRecord::Base
-end
-
-class ExamVersion < ActiveRecord::Base
-end
-
-class ExamQuestion < ActiveRecord::Base
-  belongs_to :exam_version
-end
-
-class QuestionResponse < ActiveRecord::Base
-  belongs_to :exam_question
-  before_save :normalize_answer
-
-  def normalize_answer
-    unless self.answer.blank?
-      if self.exam_question.kind == 'CHECK_ALL'
-        self.answer = self.answer.split(',').map(&:to_i).sort.join(',')
-      end
-    end
-  end
-end
-
 class NormalizeCheckAllAnswers < ActiveRecord::Migration
+  class QuestionResponse < ActiveRecord::Base
+  end
+
   def self.up
     QuestionResponse.all.each do |qr|
-      qr.answer = qr.answer.split(',').map(&:to_i).sort.join(',')
-      qr.save
+      qr.update_attributes(:answer => qr.answer.split(',').map(&:to_i).sort.join(','))
     end
   end
 
