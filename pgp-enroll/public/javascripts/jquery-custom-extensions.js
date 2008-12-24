9c83b8cad1e9d51380e64ef4cb1dfc292c6bdd74
Jason Morrison <jason.p.morrison@gmail.com>
2008-12-24 02:11:27 +0000

    Added JS to guide user through the residency survey

---
+++ pgp-enroll/public/javascripts/jquery-custom-extensions.js
@@ -0,0 +1,17 @@
+// From http://www.learningjquery.com/2007/08/clearing-form-data
+// Also included in jQuery Form Plugin http://malsup.com/jquery/form/
+
+$.fn.clearForm = function() {
+  return this.each(function() {
+  var type = this.type, tag = this.tagName.toLowerCase();
+  if (tag == 'form')
+    return $(':input',this).clearForm();
+  if (type == 'text' || type == 'password' || tag == 'textarea')
+    this.value = '';
+  else if (type == 'checkbox' || type == 'radio')
+    this.checked = false;
+  else if (tag == 'select')
+    this.selectedIndex = -1;
+  });
+};
+
