08d9414bae16741a7c6736aa7485aef8b3ec63b1
Jason Morrison <jason.p.morrison@gmail.com>
2009-02-18 00:38:47 +0000

    Added PHR upload form and basic validation

--- pgp-enroll/app/controllers/trait_collections_controller.rb
+++ pgp-enroll/app/controllers/trait_collections_controller.rb
@@ -3,8 +3,16 @@ class TraitCollectionsController < ApplicationController
   end
 
   def create
-    step = EnrollmentStep.find_by_keyword('trait_collection')
-    current_user.complete_enrollment_step(step)
-    redirect_to root_path
+    current_user.phr = params[:phr]
+
+    if current_user.save && current_user.reload.phr && current_user.reload.phr.exists?
+      step = EnrollmentStep.find_by_keyword('trait_collection')
+      current_user.complete_enrollment_step(step)
+      redirect_to root_path
+    else
+      flash[:error] = "You must upload a valid CCR file containing your personal health record to proceed."
+      show
+      render :action => 'show'
+    end
   end
 end
--- pgp-enroll/test/functional/trait_collections_controller_test.rb
+++ pgp-enroll/test/functional/trait_collections_controller_test.rb
@@ -26,8 +26,29 @@ class TraitCollectionsControllerTest < ActionController::TestCase
       end
     end
 
-    context "on POST to create" do
-      setup { post :create }
+    context "on POST to create without a file" do
+      setup do
+        phr = mock('phr')
+        phr.stubs(:exists?).returns(false)
+        User.any_instance.stubs(:phr).returns(phr)
+
+        post :create
+      end
+
+      should_not_change 'EnrollmentStepCompletion.count'
+      should_respond_with :success
+      should_render_template :show
+      should_set_the_flash_to /file/i
+    end
+
+    context "on POST to create with a file" do
+      setup do
+        phr = mock('phr')
+        phr.stubs(:exists?).returns(true)
+        User.any_instance.stubs(:phr).returns(phr)
+
+        post :create
+      end
 
       should_change 'EnrollmentStepCompletion.count', :by => 1
       should_redirect_to 'root_path'
