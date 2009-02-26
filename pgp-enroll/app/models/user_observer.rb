0c76909dc6bc6501ef904cb5c49486d52df57b1f
Jason Morrison <jason.p.morrison@gmail.com>
2009-02-26 23:08:37 +0000

    Remove account activation confirmation email (email #2)

--- pgp-enroll/app/models/user_observer.rb
+++ pgp-enroll/app/models/user_observer.rb
@@ -3,9 +3,10 @@ class UserObserver < ActiveRecord::Observer
     UserMailer.deliver_signup_notification(user)
   end
 
-  def after_save(user)
-  
-    UserMailer.deliver_activation(user) if user.recently_activated?
-  
-  end
+  # As requested by Jason Bobe in an email Feb 24
+  # and http://skitch.com/pnome/bfyqe/activation-email-2
+  # do not send activation email
+  # def after_save(user)
+  #   UserMailer.deliver_activation(user) if user.recently_activated?
+  # end
 end
