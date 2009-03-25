0ca603ff0bc86758347e21ae477af6a09de5c8cf
Jason Morrison <jason.p.morrison@gmail.com>
2009-03-25 03:27:55 +0000

    Remove sign up link from nav bar, and clear session[:invite] if you enter a non-invited URL to accepted_invites#create

--- pgp-enroll/app/controllers/accepted_invites_controller.rb
+++ pgp-enroll/app/controllers/accepted_invites_controller.rb
@@ -9,6 +9,7 @@ class AcceptedInvitesController < ApplicationController
       session[:invited] = true
       redirect_to page_url(:introduction)
     else
+      session[:invited] = false
       flash[:error] = "Sorry, that email address has not yet been invited to enroll."
       redirect_to page_url(:home)
     end
