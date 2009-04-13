21ced7794ac13931dc221b1a27274f705d02f9b1
Jason Morrison <jason.p.morrison@gmail.com>
2009-04-13 00:47:47 +0000

    Updated user reporting to include survey responses

--- pgp-enroll/app/helpers/admin/users_helper.rb
+++ pgp-enroll/app/helpers/admin/users_helper.rb
@@ -1,11 +1,39 @@
 module Admin::UsersHelper
+  # TODO: Move to a separate presenter class instead of a helper.
   def csv_for_users(users)
-    fields = %w(name email activated_at)
+    user_fields = %w(first_name last_name email activated_at).freeze
+
+    survey_response_fields = {
+      :privacy   => %w(worrisome_information_comfort_level
+                       information_disclosure_comfort_level
+                       past_genetic_test_participation),
+
+      :family    => %w(birth_year relatives_interested_in_pgp monozygotic_twin
+                       child_situation youngest_child_birth_year),
+
+      :residency => %w(us_resident country zip can_travel_to_boston)
+    }.freeze
     buf = ''
 
-    CSV.generate_row(fields.map(&:humanize), fields.size, buf)
+    header_row = user_fields.map(&:humanize)
+    survey_response_fields.each do |survey, fields|
+      header_row |= fields.map { |field| "#{survey.to_s.humanize} #{field.to_s.humanize}" }
+    end
+
+    CSV.generate_row(header_row, header_row.size, buf)
     users.each do |user|
-      CSV.generate_row(fields.map {|f| user.send(f) }, fields.size, buf)
+
+      row = []
+      row |= user_fields.map { |field| user.send(field) }
+
+      survey_response_fields.each do |survey, fields|
+        survey_response = user.send(:"#{survey}_survey_response")
+        fields.each do |field|
+          row.push(survey_response ? survey_response.send(field) : '-')
+        end
+      end
+
+      CSV.generate_row(row, row.size, buf)
     end
 
     buf
--- pgp-enroll/test/functional/admin/users_controller_test.rb
+++ pgp-enroll/test/functional/admin/users_controller_test.rb
@@ -22,43 +22,62 @@ class Admin::UsersControllerTest < ActionController::TestCase
       login_as @user
     end
 
-    context 'on GET to index' do
+    context 'with some users' do
       setup do
-        get :index
-        assert_response :success
+        5.times { Factory(:user) }
       end
 
-      should 'show all users' do
-        User.all.each do |user|
-          assert_select 'td', user.email
-        end
-      end
-    end
-
-    context 'where some, but not all, users have completed the enrollment exam' do
-      setup do
-        assert @exams_step = EnrollmentStep.find_by_keyword('content_areas')
-        @completed_users = [Factory(:user)]
-        @uncompleted_users = [Factory(:user)]
-        @completed_users.each { |u| Factory(:enrollment_step_completion, :enrollment_step => @exams_step, :user => u) }
-      end
-
-      context 'on GET to index with ?completed_enrollment_exam=true' do
+      context 'on GET to index' do
         setup do
-          get :index, :completed_enrollment_exam => true
+          get :index
+          assert_response :success
         end
 
-        should_respond_with :success
-        should_render_template :index
-
-        should 'show all users who have completed the enrollment exam' do
-          assigns(:users).each do |user|
-            assert user.completed_enrollment_steps.include?(@exams_step)
+        should 'show all users' do
+          User.all.each do |user|
+            assert_select 'td', user.email
           end
         end
+      end
 
-        should 'link to the CSV download with the same filter' do
-          assert_select 'a[href=?]', formatted_admin_users_url(:format => 'csv', :completed_enrollment_exam => true)
+      context 'on GET to index as CSV' do
+        setup do
+          get :index, :format => 'csv'
+          assert_response :success
+        end
+
+        should 'show all users' do
+          User.all.each do |user|
+            assert_select user.email
+          end
+        end
+      end
+
+      context 'where some, but not all, users have completed the enrollment exam' do
+        setup do
+          assert @exams_step = EnrollmentStep.find_by_keyword('content_areas')
+          @completed_users = [Factory(:user)]
+          @uncompleted_users = [Factory(:user)]
+          @completed_users.each { |u| Factory(:enrollment_step_completion, :enrollment_step => @exams_step, :user => u) }
+        end
+
+        context 'on GET to index with ?completed_enrollment_exam=true' do
+          setup do
+            get :index, :completed_enrollment_exam => true
+          end
+
+          should_respond_with :success
+          should_render_template :index
+
+          should 'show all users who have completed the enrollment exam' do
+            assigns(:users).each do |user|
+              assert user.completed_enrollment_steps.include?(@exams_step)
+            end
+          end
+
+          should 'link to the CSV download with the same filter' do
+            assert_select 'a[href=?]', formatted_admin_users_url(:format => 'csv', :completed_enrollment_exam => true)
+          end
         end
       end
     end
