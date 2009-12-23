b7afd49624f4edf21f5959116e6805554a266aa8
Miron Cuperman <c1.bio@niftybox.net>
2009-12-22 21:34:49 -0500

    move up

--- pgp-enroll/config/deploy.production.rb
+++
@@ -1,6 +0,0 @@
-set :application, "pgp-enroll"
-set :domain,      "www-prod.oxf"
-set :deploy_to,   "/var/www/enroll.personalgenomes.org"
-set :repository,  "http://dev.freelogy.org/svn/pgp-enroll/trunk"
-set :rails_env,   "production"
-set :config_files, ['database.yml']
--- pgp-enroll/config/deploy.rb
+++
@@ -1,6 +0,0 @@
-set :application, "pgp-enroll"
-set :domain,      "www-dev.oxf"
-set :deploy_to,   "/var/www/enroll-dev.personalgenomes.org"
-set :repository,  "http://dev.freelogy.org/svn/pgp-enroll/trunk"
-set :rails_env,   "staging"
-set :config_files, ['database.yml']
