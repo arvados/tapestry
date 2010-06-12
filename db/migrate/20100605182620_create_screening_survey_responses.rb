4229a4cf69298f14152883718390dd6e974407a4
Ward Vandewege <ward@scalablecomputingexperts.com>
2010-06-12 07:58:24 -0400

    migrate data from screening survey v1 to v2

--- db/migrate/20100605182620_create_screening_survey_responses.rb
+++ db/migrate/20100605182620_create_screening_survey_responses.rb
@@ -11,6 +11,25 @@ class CreateScreeningSurveyResponses < ActiveRecord::Migration
 
       t.timestamps
     end
+    User.find(:all).each do |u|
+      if (u.residency_survey_response or u.family_survey_response or u.privacy_survey_response) then
+        u.screening_survey_response = ScreeningSurveyResponse.new()
+        s = u.screening_survey_response
+        if (u.residency_survey_response) then
+          s.us_resident = u.residency_survey_response.us_resident
+        end
+        if (u.family_survey_response) then
+          s.age_21 = u.family_survey_response.birth_year < (Time.now.year - 21)
+          s.monozygotic_twin = u.family_survey_response.monozygotic_twin
+        end
+        if (u.privacy_survey_response) then
+          s.worrisome_information_comfort_level = u.privacy_survey_response.worrisome_information_comfort_level
+          s.information_disclosure_comfort_level = u.privacy_survey_response.information_disclosure_comfort_level
+          s.past_genetic_test_participation = u.privacy_survey_response.past_genetic_test_participation
+        end
+        s.save!
+      end
+    end
   end
 
   def self.down
