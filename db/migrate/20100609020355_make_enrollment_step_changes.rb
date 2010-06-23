bee0e4c38f0c412d1a40f6f00c42d4bd9ed17d4d
Ward Vandewege <ward@scalablecomputingexperts.com>
2010-06-22 22:34:25 -0400

    more enrollment step changes

--- db/migrate/20100609020355_make_enrollment_step_changes.rb
+++ db/migrate/20100609020355_make_enrollment_step_changes.rb
@@ -2,14 +2,16 @@ class MakeEnrollmentStepChanges < ActiveRecord::Migration
   def self.up
     # Insert a new step to show the eligibility results
     execute "UPDATE enrollment_steps set ordinal=ordinal+1 where ordinal>2"
-    execute "INSERT INTO enrollment_steps (keyword, ordinal, title, description) values ('screening_survey_results',3,'Eligibility Questionnaire Results','Eligibility Questionnaire Results')"
+    execute "INSERT INTO enrollment_steps (keyword, ordinal, title, description, duration) values ('screening_survey_results',3,'Eligibility Questionnaire Results','Eligibility Questionnaire Results','instant')"
     # Change duration for the consent review step
     execute "UPDATE enrollment_steps set duration='30 minutes' where keyword='consent_review'"
     # Change step 6 to 'enrollment queue'
     execute "UPDATE enrollment_steps set duration='4-6 weeks',keyword='enrollment_queue',title='Enrollment Queue',description='Enrollment Queue' where keyword='screening_submission'"
     # Drop other steps
     execute "DELETE from enrollment_steps where keyword='eligibility_screening_results'"
-    execute "DELETE from enrollment_steps where ordinal>=9"
+    execute "DELETE from enrollment_steps where ordinal>=9 and ordinal <=12"
+    execute "UPDATE enrollment_steps set ordinal='11' where keyword='enrollment_application'"
+    execute "UPDATE enrollment_steps set ordinal='12',keyword='enrollment_application_results',duration='1-2 months'  where keyword='eligibility_application_results'"
     # Change ordinal for 'Consent to participate' step
     execute "UPDATE enrollment_steps set ordinal='7', duration='30-60 minutes' where keyword='participation_consent'"
 end
@@ -28,8 +30,8 @@ end
     execute "INSERT INTO enrollment_steps (duration, keyword, ordinal, title, description) values ('2 minutes','trait_collection',9,'Baseline Traits Survey','Collect your baseline traits')"
     execute "INSERT INTO enrollment_steps (duration, keyword, ordinal, title, description) values ('1 minute','pledge',10,'Make Financial Pledge','Make Financial Pledge')"
     execute "INSERT INTO enrollment_steps (duration, keyword, ordinal, title, description) values ('1 hour','distinctive_traits_survey',11,'Distinctive traits survey','Enter detailed traits')"
-    execute "INSERT INTO enrollment_steps (duration, keyword, ordinal, title, description) values ('1 minute','enrollment_application',12,'Submit Enrollment Application','Submit Enrollment Application')"
-    execute "INSERT INTO enrollment_steps (duration, keyword, ordinal, title, description) values ('2-4 weeks','eligibility_application_results',13,'Enrollment Application Results','Enrollment Application Results')"
+    execute "UPDATE enrollment_steps set ordinal='12' where keyword='enrollment_application'"
+    execute "UPDATE enrollment_steps set ordinal='13',keyword='eligibility_application_results',duration='2-4 weeks' where keyword='enrollment_application_results'"
     # Change ordinal for 'Consent to participate' step
     execute "UPDATE enrollment_steps set ordinal='7', duration='5 minutes' where keyword='participation_consent'"
   end
