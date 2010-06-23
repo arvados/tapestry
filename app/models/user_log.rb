312e5ef9a2030549925a60dd8888c64f846e85be
Ward Vandewege <ward@scalablecomputingexperts.com>
2010-06-22 22:31:53 -0400

    add constraint on user_log model

--- app/models/user_log.rb
+++ app/models/user_log.rb
@@ -1,5 +1,8 @@
 class UserLog < ActiveRecord::Base
   belongs_to :user
   belongs_to :enrollment_step
+
+  validates_presence_of :user_id
+
   attr_accessible :user, :comment, :enrollment_step
  end
