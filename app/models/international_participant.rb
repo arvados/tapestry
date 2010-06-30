b5d5df9217fb5bfed31aa68c9ae45059e411959e
Ward Vandewege <ward@scalablecomputingexperts.com>
2010-07-01 00:07:56 +0200

    better wording on e-mail address validation

--- app/models/international_participant.rb
+++ app/models/international_participant.rb
@@ -2,7 +2,7 @@ class InternationalParticipant < ActiveRecord::Base
   validates_length_of       :country,    :within => 3..100
 
   validates_length_of       :email,    :within => 6..100 #r@a.wk
-  validates_uniqueness_of   :email,    :case_sensitive => false
+  validates_uniqueness_of   :email,    :case_sensitive => false, :message => "address is already registered"
   validates_format_of       :email,    :with => /.+@.+\...+/, :message => " address is invalid"
 
 end
