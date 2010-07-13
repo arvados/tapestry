6d57d7f4377268ca5ced31b1c3a6b65fc9d8b142
Si Wong <siywong@pgp-enroll-dev.(none)>
2010-07-12 17:00:01 -0700

    Google Health changes

---
+++ app/controllers/phrccrs_controller.rb
@@ -0,0 +1,103 @@
+class PhrccrsController < ApplicationController
+  include PhrccrsHelper
+  attr_accessor :ccr
+  attr_accessor :outFile
+  before_filter :store_location
+  attr_accessor :processing_time
+  attr_accessor :download_time
+
+  def show
+  end
+
+  def review
+    ccr_path = get_ccr_filename(current_user.id, false)
+    if !File.exist?(ccr_path)
+      feed = get_ccr(current_user)
+    else
+      feed = File.new(ccr_path)
+    end
+    @ccr = Nokogiri::XML(feed)
+  end
+
+  def create
+    commit_action = params[:commit]
+    if commit_action.eql?('Link Profile')
+       authsub()
+    elsif commit_action.eql?('Unlink Profile')
+       authsub_revoke(current_user)
+       flash[:notice] = 'Your profile has been successfully unlinked'
+       redirect_to :action => :show
+    elsif commit_action.eql?('Review PHR')
+       redirect_to :controller => 'phrccrs', :action => 'review'
+    elsif commit_action.eql?('Share my PHR with PGP')
+       download_phr()
+       flash[:notice] = 'Your PHR has been shared with the PGP.'
+       redirect_to '/'
+    elsif commit_action.eql?('Update PHR')
+       download_phr()
+       redirect_to :action => :review
+    else
+       redirect_to :action => :show
+    end
+  end
+
+  def unlink_googlehealth
+    authsub_revoke(current_user)
+    redirect_to edit_user_url(current_user)
+  end
+
+  def authsub_update
+    if !params[:token].blank?
+      begin
+	authsubRequest = GData::Auth::AuthSub.new(params[:token])
+        authsubRequest.private_key = private_key
+	authsubRequest.upgrade
+	
+	current_user.update_attributes(:authsub_token => authsubRequest.token)
+	flash[:notice] = 'Your Google Health Profile was successfully linked'
+	redirect_to :action => :show
+      rescue GData::Client::Error => ex
+	flash[:error] = 'We could not link your Google Health profile. Please try again.'
+	redirect_to :action => :show
+      end
+    else
+      flash[:error] = 'No token provided'
+      redirect_to :action => :show
+    end
+  end
+
+  def authsub
+    if ROOT_URL == "enroll-si.personalgenomes.org"
+      scope = 'https://www.google.com/h9/feeds'
+      next_url = 'http://enroll-si.personalgenomes.org/phrccr/authsub'
+    elsif ROOT_URL == "enroll-dev.personalgenomes.org"
+      scope = 'https://www.google.com/h9/feeds'
+      next_url = 'http://enroll-dev.personalgenomes.org/phrccr/authsub'
+    else
+      scope = 'https://www.google.com/health/feeds'
+      next_url = 'https://enroll.personalgenomes.org/phrccr/authsub'
+    end
+    next_url = 'http://enroll-si.personalgenomes.org/phrccr/authsub'
+    secure = true  # set secure = true for signed AuthSub requests
+    sess = 1
+    authsub_link = AuthSub.get_url(next_url, scope, secure, sess)
+    redirect_to authsub_link    
+  end
+
+  def download_phr
+    feed = get_ccr(current_user)
+    begin
+      outFile = File.new(get_ccr_filename(current_user.id), 'w')
+      outFile.write(feed)
+      outFile.close
+    rescue
+      flash[:error] = 'There was an error saving your PHR.'
+      redirect_to :action => :review
+    else
+      #Not sure if this should advance enrollment step
+      #current_user.complete_enrollment_step(current_user.next_enrollment_step)
+      #Do not revoke yet, keep for updating each time user logs in
+      #authsub_revoke
+    end
+  end
+end
---
+++ app/helpers/phrccrs_helper.rb
@@ -0,0 +1,168 @@
+# Methods added to this helper will be available to all templates in the application.
+# Copyright (C) 2008 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'cgi'
+require 'openssl'
+require 'base64'
+
+module PhrccrsHelper
+    # This is a modification of the AuthSub class to handle generate correct Health/H9 requests
+    # This class implements AuthSub signatures for Data API requests.
+    # It can be used with a GData::Client::GData object.
+    class AuthSub
+
+      # The URL of AuthSubRequest.
+      H9_REQUEST_HANDLER = 'https://www.google.com/h9/authsub'
+      HEALTH_REQUEST_HANDLER = 'https://www.google.com/health/authsub'
+
+      # Return the proper URL for an AuthSub approval page with the requested
+      # scope. next_url should be a URL that points back to your code that
+      # will receive the token. domain is optionally a Google Apps domain.
+      def self.get_url(next_url, scope, secure = false, session = true,
+          domain = nil)
+        next_url = CGI.escape(next_url)
+        scope = CGI.escape(scope)
+        secure = secure ? 1 : 0
+        session = session ? 1 : 0
+        body = "next=#{next_url}&scope=#{scope}&session=#{session}" +
+               "&secure=#{secure}"
+        if domain
+          domain = CGI.escape(domain)
+          body = "#{body}&hd=#{domain}"
+        end
+        if scope.index('h9')
+          return "#{H9_REQUEST_HANDLER}?#{body}&permission=1"
+        else
+          return "#{HEALTH_REQUEST_HANDLER}?#{body}&permission=1"
+        end
+      end
+  end
+
+  # View helper method to display DOB and Age
+  def dob_to_dob_age(dob_s)
+    if (dob_s && !dob_s.empty?)
+      dob = Date.parse(dob_s.text)
+      now = DateTime.now
+      a = now.year - dob.year - 1
+      if now.month > dob.month || now.month == dob.month && now.day >= dob.day
+        a = a + 1
+      end
+      return dob_s.text + ' (' + a.to_s + ' years old)'
+   end
+   return ''
+  end
+
+  # View helper method to display weight in pounds and kilograms
+  def oz_to_lbs_kg(oz)
+    if (oz && !oz.empty?)
+      oz = oz.text.to_i
+      return (oz / 16).to_s + 'lbs (' + (oz / 35.2739619).to_i.to_s + 'kg)'
+    end
+    return ''
+  end
+
+  # View helper method to display height in feet and centimeters
+  def in_to_ft_in_cm(inches)
+    if (inches && !inches.empty?)
+      inches = inches.text.to_i
+      ft = [inches / 12, inches % 12]
+      s = ft[0].to_s + 'ft'
+      if ft[1] > 0
+        s = s + ' ' + ft[1].to_s + 'in'
+      end
+      return s + ' (' + (inches / 0.393700787).to_i.to_s + 'cm)'
+    end
+    return ''
+  end
+
+  def dose_frequency(dose, frequency)
+    s = ''
+    if dose && !dose.empty?
+      s = 'Take ' + dose.text
+    end
+    if dose && !dose.empty? && frequency && !frequency.empty?
+      s = s + ', '
+    end
+    if frequency && !frequency.empty?
+      s = s + frequency.text
+    end
+    return s
+  end
+
+  # Returns filename of ccr based on user's id
+  # File is based on a left-padded user id divided into 2 digit chunks
+  # e.g. User id : 12345 => 01/23/45/ccr.xml
+  #      User id : 314159 => 31/41/59/ccr.xml
+  def get_ccr_filename(user_id, create_dir = true)
+    user_id = user_id.to_s
+    if user_id.length % 2 == 1
+      user_id = '0' + user_id
+    end
+    f = 'ccr/'
+
+    while user_id.length > 0
+      f = f + user_id[0,2]
+      if create_dir && !File.directory?(f)
+        Dir.mkdir(f)
+      end
+      f = f + '/'
+      user_id = user_id[2, user_id.length]
+    end
+
+    return f + '/ccr.xml'
+  end
+
+  def health_url
+    if ROOT_URL == "enroll.personalgenomes.org"
+      return "https://www.google.com/health"
+    else
+      return "https://www.google.com/h9"
+    end
+  end
+
+  def show_date(n)
+    unless n && n.length > 0
+      return ''
+    end
+    return n[0].inner_text[0,10]
+  end
+
+  # Returns location of private key used to sign Google Health requests
+  def private_key
+    return 'config/pgpenrollkey.pem'
+  end
+
+  def ccr_profile_url
+    return health_url + '/feeds/profile/default'
+  end
+
+  def authsub_revoke(current_user)
+    authsubRequest = GData::Auth::AuthSub.new(current_user.authsub_token)
+    authsubRequest.private_key = private_key
+    authsubRequest.revoke
+  rescue GData::Client::Error => ex
+    #Ignore AuthorizationError because it most likely means the token was invalidated by the user through Google
+  ensure
+    current_user.update_attributes(:authsub_token => '')   
+  end
+
+  def get_ccr(current_user)
+    client = GData::Client::Base.new
+    client.authsub_token = current_user.authsub_token
+    client.authsub_private_key = private_key
+    feed = client.get(ccr_profile_url).body
+    return feed
+  end
+end
