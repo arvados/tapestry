9749c74d40323e4c4c32dc84ce113927eb2e8764
Ward Vandewege <ward@scalablecomputingexperts.com>
2010-10-15 11:48:40 -0400

    quick script to make a list of enrolled users without PHR - output tweak

--- script/find_users_without_phr.rb
+++ script/find_users_without_phr.rb
@@ -8,7 +8,7 @@ ENV["RAILS_ENV"] = "production" if production
 require File.dirname(__FILE__) + '/../config/boot'
 require File.dirname(__FILE__) + '/../config/environment'
 
-# List name/e-mail address for enrolled users for whom we do not have a PHR.
+# List e-mail address for enrolled users for whom we do not have a PHR.
 # 
 # Ward, 2010-10-15
   
@@ -24,13 +24,13 @@ User.enrolled.each do |u|
   ccr_list = Dir.glob(get_ccr_path(u.id) + '*').reverse
   ccr_list.delete_if { |s| true if not File.file?(s) or s.scan(/.+\/ccr(.+)\.xml/).empty? }
   if ccr_list.length == 0 then
-    puts "#{u.full_name} <#{u.email}>"
+    puts u.email
     count += 1
     next
   end 
   ccr_history = ccr_list.map { |s| s.scan(/.+\/ccr(.+)\.xml/)[0][0] }
   if ccr_history.length == 0 then
-    puts "#{u.full_name} <#{u.email}>"
+    puts u.email
     count += 1
     next
   end
