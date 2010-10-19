23952b7f2dd9bf6702de7a140a84456d85c33aca
Ward Vandewege <ward@scalablecomputingexperts.com>
2010-10-19 10:10:48 -0400

    resetting password succesfully should also activate the uesr if they are not active yet

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -36,6 +36,13 @@ class PasswordsController < ApplicationController
       if @user.save
         flash[:notice] = 'You reset your password successfully.'
         @user.log("Password reset successfully")
+        # Some people never activated their accounts (verified e-mail addres):
+        # they never got the e-mail, or didn't click the link, or... If they
+        # got here, they got the password activation link, so their e-mail
+        # address is verified. Activate them so they can actually log in.
+        if !@user.active? then
+          @user.activate!
+        end
         redirect_to login_url
       else
         render :action => 'edit'
