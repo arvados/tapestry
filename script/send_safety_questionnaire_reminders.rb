634cf75e2a6c7545d141f8a2a6c872f573e2d4d8
Ward Vandewege <ward@scalablecomputingexperts.com>
2010-10-21 09:17:52 -0400

    make send_safety_questionnaire_reminders.rb script print a line to STDOUT when it sends a message so that we can keep an eye on the cron job, at least initially

--- script/send_safety_questionnaire_reminders.rb
+++ script/send_safety_questionnaire_reminders.rb
@@ -14,13 +14,14 @@ require File.dirname(__FILE__) + '/../config/boot'
 require File.dirname(__FILE__) + '/../config/environment'
 
 User.find(:all).each do |u|
-	next if u.enrolled.nil?	# shortcut for speed
+  next if u.enrolled.nil? # shortcut for speed
   if (u.user_logs.find_by_comment('Sent Safety Questionnaire Reminder').nil?) or 
      (u.user_logs.find_by_comment('Sent Safety Questionnaire Reminder') and
       2.weeks.ago > u.user_logs.find(:last, :conditions => "comment = 'Sent Safety Questionnaire Reminder'").created_at) then
     if not u.has_recent_safety_questionnaire then
       UserMailer.deliver_safety_questionnaire_reminder(u)
       u.log("Sent Safety Questionnaire Reminder")
+      puts "Sent Safety Questionnaire Reminder for #{u.full_name} (#{u.id})"
     end
   end
 end
