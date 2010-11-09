cca0c2fc3ab1b6b812d8e892f9ceee832d35099f
Ward Vandewege <ward@scalablecomputingexperts.com>
2010-11-09 15:51:57 -0500

    sort international scoreboard by count, descending

--- app/controllers/admin/scoreboards_controller.rb
+++ app/controllers/admin/scoreboards_controller.rb
@@ -1,6 +1,6 @@
 class Admin::ScoreboardsController < Admin::AdminControllerBase
 
   def index 
-    @internationalUserCount = InternationalParticipant.find_by_sql("SELECT country, count(*) AS count FROM international_participants GROUP BY country")     
+    @internationalUserCount = InternationalParticipant.find_by_sql("SELECT country, count(*) AS count FROM international_participants GROUP BY country ORDER BY count desc")
   end
 end
