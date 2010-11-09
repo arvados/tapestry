bea0282e198e3dd61a761598111028bce2be2f9e
Si Wong <siywong@pgp-enroll-dev.(none)>
2010-11-09 07:29:52 -0800

    international users scoreboard

---
+++ app/controllers/admin/scoreboards_controller.rb
@@ -0,0 +1,6 @@
+class Admin::ScoreboardsController < Admin::AdminControllerBase
+
+  def index 
+    @internationalUserCount = InternationalParticipant.find_by_sql("SELECT country, count(*) AS count FROM international_participants GROUP BY country")     
+  end
+end
