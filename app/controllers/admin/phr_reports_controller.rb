b13c4cffd7463bc678e25e4d23586a12c8d3d51e
Si Wong <siywong@pgp-enroll-dev.(none)>
2010-11-10 13:40:17 -0800

    fixed unqualified column name in phr reports sql

--- app/controllers/admin/phr_reports_controller.rb
+++ app/controllers/admin/phr_reports_controller.rb
@@ -73,7 +73,7 @@ class Admin::PhrReportsController < Admin::AdminControllerBase
     @users = []
     skipped = 0
     unless params[:commit].nil? or queries.empty? or query_params.empty? 
-      ccr_results = Ccr.find(:all, :joins => joins.join(' '), :conditions => ['(' + queries.join(') AND (') + ')', query_params].flatten, :order => 'id, version', :group => 'user_id', :include => :user)
+      ccr_results = Ccr.find(:all, :joins => joins.join(' '), :conditions => ['(' + queries.join(') AND (') + ')', query_params].flatten, :order => 'ccrs.id, version', :group => 'user_id', :include => :user)
       ccr_results.each {|ccr|
         unless ccr.user.nil? or ccr.user.is_test
           @ccrs << ccr
