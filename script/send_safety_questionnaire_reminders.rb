aa69d2837540ab2903ea8ca4e36bf50a23293b90
Ward Vandewege <ward@scalablecomputingexperts.com>
2011-04-04 20:20:04 -0400

    send safety questionnaire reminders once every 6 weeks instead of once every 2 weeks

--- script/send_safety_questionnaire_reminders.rb
+++ script/send_safety_questionnaire_reminders.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 # Send out Safety Questionnaire reminders for people who have not filled one
-# out in the last 3 months. Repeat reminders every 2 weeks. 
+# out in the last 3 months. Repeat reminders every 6 weeks. 
 #
 # Ward, 2010-09-08
 
@@ -17,7 +17,7 @@ User.find(:all).each do |u|
   next if u.enrolled.nil? # shortcut for speed
   if (u.user_logs.find_by_comment('Sent PGP Safety Questionnaire Reminder').nil?) or 
      (u.user_logs.find_by_comment('Sent PGP Safety Questionnaire Reminder') and
-      2.weeks.ago > u.user_logs.find(:last, :conditions => "comment = 'Sent PGP Safety Questionnaire Reminder'").created_at) then
+      6.weeks.ago > u.user_logs.find(:last, :conditions => "comment = 'Sent PGP Safety Questionnaire Reminder'").created_at) then
     if not u.has_recent_safety_questionnaire then
       # The UserMailer call will also add the 'Sent PGP Safety Questionnaire Reminder' user log entry
       UserMailer.deliver_safety_questionnaire_reminder(u)
