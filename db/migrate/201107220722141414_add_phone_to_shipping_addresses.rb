cfc0d5450083d03035921336fc8c621cd6daac98
Tom Clegg <tom@clinicalfuture.com>
2011-07-25 18:40:50 -0400

    fix typo in down-migration

--- db/migrate/201107220722141414_add_phone_to_shipping_addresses.rb
+++ db/migrate/201107220722141414_add_phone_to_shipping_addresses.rb
@@ -6,6 +6,6 @@ class AddPhoneToShippingAddresses < ActiveRecord::Migration
 
   def self.down
     remove_column :shipping_address_versions, :phone
-    remove_column :shipping_addressess, :phone
+    remove_column :shipping_addresses, :phone
   end
 end
