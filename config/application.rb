1567797a1db8fa8c6aefe5157a7e2f8e23d18fce
Ward Vandewege <ward@jhvc.com>
2011-07-27 14:50:54 -0400

    update jquery, and fix delete user link on the admin interface

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module PgpEnroll
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
+    config.action_view.javascript_expansions[:defaults] = %w(jquery-ui-1.8.11.custom.min jquery-1.6.2.min jquery-custom-extensions rails)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
