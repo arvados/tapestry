1f8e8adcd6683350cc1f95e2d9609f6f1541a562
Ward Vandewege <ward@jhvc.com>
2011-08-01 15:33:03 -0400

    Revert 5eee1edf667b4d41e6fd2f41568aac79bdc05fd2 and now really fix #634 properly.

--- app/models/unused_kit_name.rb
+++ app/models/unused_kit_name.rb
@@ -3,9 +3,7 @@ class UnusedKitName < ActiveRecord::Base
   acts_as_paranoid_versioned :version_column => :lock_version
 
   def self.random
-    name = self.first(:order => 'RAND()')
-    name = '' if name.nil? 
-    return name
+    self.first(:order => 'RAND()')
   end
 
 end
