6476d41b675b7bb9d07eedd8d64f0f9bc6e99355
Ward Vandewege <ward@jhvc.com>
2011-08-02 15:31:00 -0400

    move require statements out of config/environment.rb and into an initializer

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 PgpEnroll::Application.initialize!
-
-require 'stringio'
-require 'csv'
-require 'drb'
-
-# Recaptcha gem has funky file naming - the stuff we need is in the rails.rb file
-require "recaptcha/rails"
