524c3f6b73834ca3832538fd988ea8c2635ac848
Ward Vandewege <ward@jhvc.com>
2011-08-02 22:53:58 -0400

    add script to synchronize google surveys

---
+++ script/synchronize_google_surveys.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+# Default is development
+production = ARGV[0] == "production"
+
+ENV["RAILS_ENV"] = "production" if production
+
+require File.dirname(__FILE__) + '/../config/boot'
+require File.dirname(__FILE__) + '/../config/environment'
+
+GoogleSurvey.find(1).synchronize!
