1223c2119bc52a0c7cd6ef62c84f88c53caae99d
Tom Clegg <tom@clinicalfuture.com>
2011-08-11 21:27:23 -0400

    show google survey results in Completed Surveys section of public profile

--- app/models/google_survey_answer.rb
+++ app/models/google_survey_answer.rb
@@ -1,5 +1,9 @@
 class GoogleSurveyAnswer < ActiveRecord::Base
   belongs_to :google_survey
+  has_one :google_survey_question,
+    :foreign_key => 'google_survey_id',
+    :primary_key => :google_survey_id,
+    :conditions => [' `column` = ? ', '#{self.column}']
   belongs_to :nonce
   validates_uniqueness_of :column, :scope => :nonce_id
 end
