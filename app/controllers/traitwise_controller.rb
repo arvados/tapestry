88e61125df3ce9bc9e1c28af3ef81f5b22289838
Ward Vandewege <ward@clinicalfuture.com>
2011-09-21 13:00:07 -0400

    Handle communications errors with Traitwise more gracefully.
    This closes #590, #593 and #598.

--- app/controllers/traitwise_controller.rb
+++ app/controllers/traitwise_controller.rb
@@ -1,6 +1,8 @@
 
-# COPYRIGHT TRAITWISE
+# Parts COPYRIGHT TRAITWISE
 # LICENSE ???
+# Parts copyright Ward Vandewege, 2011
+# Same license as entire project
 
 class TraitwiseController < ApplicationController
   protect_from_forgery :only => [:index]
@@ -9,8 +11,15 @@ class TraitwiseController < ApplicationController
     @stream_from_tw = Traitwise.stream( current_user.hex, request, cookies, current_user )
   end
 
-	def proxy
-		render :text=>Traitwise.proxy( params[:q], request, cookies )
-	end
+  def proxy
+    begin
+      render :text=>Traitwise.proxy( params[:q], request, cookies )
+    rescue Exception => e
+      current_user.log("Error connecting to the Traitwise service: #{e.exception} #{e.inspect()}")
+      flash[:error] = 'There was an error connecting to the Traitwise service. Please try again later.'
+      redirect_to root_path
+      return
+    end
+  end
 
 end
