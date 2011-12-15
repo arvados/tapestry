dd0aeb45a798bd647a5fd407be9064e04d2010c5
Ward Vandewege <ward@clinicalfuture.com>
2011-12-15 10:06:50 -0500

    Fix for list of users ineligible to be enrolled (who failed the eligibility survey), to make it show all users in that situation.
    Refers to bug #804

--- app/models/user.rb
+++ app/models/user.rb
@@ -125,12 +125,12 @@ class User < ActiveRecord::Base
 
   scope :failed_eligibility_survey, lambda { 
     joins = [:enrollment_step_completions, :screening_survey_response]
-    ssr_step_id = EnrollmentStep.find_by_keyword('screening_survey_results').id
+    ss_step_id = EnrollmentStep.find_by_keyword('screening_surveys').id
     conditions_sql = "users.is_test = 'f' and users.enrolled IS NULL and 
         (screening_survey_responses.monozygotic_twin != 'no' or 
          screening_survey_responses.us_citizen_or_resident is null or 
          screening_survey_responses.us_citizen_or_resident!=1) and
-        enrollment_step_completions.enrollment_step_id=#{ssr_step_id}"
+        enrollment_step_completions.enrollment_step_id=#{ss_step_id}"
     { 
       :conditions => conditions_sql,
       :order => 'enrollment_step_completions.created_at',
