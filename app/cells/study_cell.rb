3ed6db4bfd39165e6d723269204e14440b025655
Tom Clegg <tom@clinicalfuture.com>
2012-06-04 13:38:34 -0400

    omit sections instead of crashing when not logged in. refs #920

--- app/cells/study_cell.rb
+++ app/cells/study_cell.rb
@@ -6,7 +6,7 @@ class StudyCell < Cell::Rails
   def dashboard_summary(options)
     @user = options[:user]
     @studies_pending = Study.approved.select(&:is_open?).select do |s|
-      sp = s.study_participants.where('user_id = ?', @user.id).first
+      sp = s.study_participants.where('user_id = ?', @user.id).first if @user
       sp.nil? or sp.is_undecided?
     end
     render
