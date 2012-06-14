2d83375754ee22e691dcd502be2bb47d984ad5f7
Tom Clegg <tom@clinicalfuture.com>
2012-06-14 15:03:47 -0400

    add "user-uploaded files" section to dashboard. refs #920

---
+++ app/cells/user_files_cell.rb
@@ -0,0 +1,12 @@
+class UserFilesCell < Cell::Rails
+
+  include ApplicationHelper
+  helper_method :humanize_date
+  helper_method :n_things
+
+  def dashboard_summary(options)
+    @user = options[:user]
+    render
+  end
+
+end
