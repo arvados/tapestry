dac21369747bd2f8a7ec0ce49ea58550c6776bf1
Tom Clegg <tom@clinicalfuture.com>
2012-07-18 23:31:40 -0400

    check StudyParticipant.kit_last_sent_at in "need to send kit?" rules. refs #978 refs #982

--- app/controllers/admin/researchers_controller.rb
+++ app/controllers/admin/researchers_controller.rb
@@ -30,8 +30,9 @@ class Admin::ResearchersController < Admin::AdminControllerBase
         if params['f'].include?('kits_sent') then
           # Ruby 1.9.2 introduces Array.keep_if
           @study_participants.delete_if { |p|
-            ! Kit.study(@study.id).participant(p.user.id).size.send(params['op']['kits_sent'],params['v']['kits_sent'].to_i)
-          }
+            ! Kit.study(@study.id).participant(p.user.id).size.send(params['op']['kits_sent'],params['v']['kits_sent'].to_i) ||
+            ! @study.study_participants.where('user_id=? and kit_last_sent_at', p.user.id).size.send(params['op']['kits_sent'],params['v']['kits_sent'].to_i)
+           }
         end
         respond_to do |format|
           format.csv { send_data csv_for_study_worker(@study,@study_participants), {
