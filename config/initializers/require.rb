0c0510e8b65eddaac4713ef9ceef4a6cae33361c
Tom Clegg <tom@clinicalfuture.com>
2012-07-26 11:52:30 -0400

    require md5

--- config/initializers/require.rb
+++ config/initializers/require.rb
@@ -2,6 +2,7 @@ require 'stringio'
 require 'csv'
 require 'drb'
 require 'ostruct'
+require 'md5'
 
 # Recaptcha gem has funky file naming - the stuff we need is in the rails.rb file
 require "recaptcha/rails"
