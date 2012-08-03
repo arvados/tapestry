2ff7061af42618f534636ab498e513a16ffc1d59
Ward Vandewege <ward@clinicalfuture.com>
2012-08-03 16:31:44 -0400

    Make pgp_id column migration more robust.

--- db/migrate/20120803140748_change_pgp_id_column_type.rb
+++ db/migrate/20120803140748_change_pgp_id_column_type.rb
@@ -1,9 +1,11 @@
 class ChangePgpIdColumnType < ActiveRecord::Migration
   def self.up
     User.where('pgp_id is not null').each do |u|
-      id = /^PGP(\d)/.match(u.pgp_id)
-      u.pgp_id = id[1]
-      u.save!
+      id = /^PGP(\d+)/.match(u.pgp_id)
+      if not id.nil? then
+        u.pgp_id = id[1]
+        u.save!
+      end
     end
     change_table :users do |t|
       t.change :pgp_id, :integer
