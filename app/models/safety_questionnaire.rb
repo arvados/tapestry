7c1aa887ee49886aef822c365b16d39e552be293
Ward Vandewege <ward@clinicalfuture.com>
2012-08-14 10:09:23 -0400

    Improve UI for safety questionnaire.
    This closes #441

--- app/models/safety_questionnaire.rb
+++ app/models/safety_questionnaire.rb
@@ -4,8 +4,21 @@ class SafetyQuestionnaire < ActiveRecord::Base
 
   belongs_to :user
 
-  validates_presence_of :user_id
-  validates_inclusion_of :has_changes, :in => [ true, false ]
-  validates_presence_of :datetime
+  validates :user_id, :datetime, :presence => true
+  validate :must_choose_has_changes_value
+  validate :must_add_comments_if_reporting_changes
+
+  def must_choose_has_changes_value
+    # Sigh, not using validates :has_changes, :inclusion => { :in => [ true, false ], :message => '...' } because that way we can't omit the mention of 'has_changes', the field name. 
+    if has_changes.nil? or (has_changes != true and has_changes != false) then
+      errors.add(:base,"Please indicate if you would like to report changes to the PGP.")
+    end
+  end
+
+  def must_add_comments_if_reporting_changes
+    if has_changes and events == '' and reactions == '' and contact == '' and healthcare == '' then
+      errors.add(:base,"If you would like to report changes to the PGP, please add a comment to one or more of the questions. Otherwise, please select 'No, I would like to report no changes to the PGP.' in question 1.")
+    end
+  end
 
 end
