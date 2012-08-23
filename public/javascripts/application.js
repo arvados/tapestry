05b7fa0a910ee0bb516cb75da3b96c792c24f22e
Tom Clegg <tom@clinicalfuture.com>
2012-08-23 10:23:47 -0400

    fix "other data type -> specify" javascript

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,32 +1,22 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+jQuery(document).ready(function($) {
 
-(function() {
-
-  jQuery(function() {
     $("#user_file_data_type").change(function() {
-      if ($(this).val() === 'other') {
-        return $("#other_data_type_div").show();
-      } else {
-        return $("#other_data_type_div").hide();
-      }
+	if ($(this).val() === 'other') {
+            return $("#other_data_type_div").show();
+	} else {
+            return $("#other_data_type_div").hide();
+	}
     }).trigger('change');
-  });
 
-  // Kick dataTables so it refilters after the user cuts or pastes in
-  // the search box (i.e., changes val without using the keyboard).
-  (function($) {
+    // Kick dataTables so it refilters after the user cuts or pastes in
+    // the search box (i.e., changes val without using the keyboard).
     $(document).delegate('.dataTables_filter input', 'paste cut', function(e, ui) {
-      $(this).trigger('keyup', [ui]);
+	$(this).trigger('keyup', [ui]);
     });
-  })(jQuery);
 
-}).call(this);
-
-
-jQuery(function(){
-    var $ = jQuery.noConflict();
     var hidsome = false;
     $('div.user-dashboard-summary>*').each(function() {
 	if ($(this).find('.highlight-nextstep').length == 0) {
