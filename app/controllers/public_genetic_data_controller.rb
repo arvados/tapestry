732ab1088ee9ea7a26f5ab1cadea3ba0505110d7
Tom Clegg <tom@clinicalfuture.com>
2012-08-30 21:14:02 -0400

    group data_type==other on public_genetic_data pages. refs #1006

--- app/controllers/public_genetic_data_controller.rb
+++ app/controllers/public_genetic_data_controller.rb
@@ -6,18 +6,21 @@ class PublicGeneticDataController < ApplicationController
   def index
     @data_type_options = []
     @data_type_options << ['All data types', nil]
+    @known_data_type = {}
     UserFile::DATA_TYPES.each { |k,v|
       if v == 'other'
         @data_type_options << [v, v]
       else
         @data_type_options << [k, v]
       end
+      @known_data_type[v] = true
     }
     @datasets = UserFile.joins(:user).merge(User.enrolled.not_suspended).includes(:user) |
       Dataset.published.joins(:participant).merge(User.enrolled.not_suspended).includes(:participant)
     if params[:data_type] and !params[:data_type].empty?
       @datasets.reject! { |d|
-        ![d.data_type, d.class.to_s].index(params[:data_type])
+        ![d.data_type, d.class.to_s].index(params[:data_type]) and
+        !(params[:data_type] == 'other' and !@known_data_type[d.data_type])
       }
     end
     @datasets.sort! { |b,a|
@@ -42,8 +45,14 @@ class PublicGeneticDataController < ApplicationController
     @datasets = UserFile.joins(:user).merge(User.enrolled.not_suspended).includes(:user) |
       Dataset.published.joins(:participant).merge(User.enrolled.not_suspended).includes(:participant)
     @data_type_stats = {}
+    @data_type_name = {}
+    UserFile::DATA_TYPES.each do |longversion, shortversion|
+      @data_type_name[shortversion] = longversion
+    end
     @datasets.each do |d|
-      stats = @data_type_stats[d.data_type] ||= {
+      data_type = d.data_type
+      data_type = 'other' unless @data_type_name.has_key? data_type
+      stats = @data_type_stats[data_type] ||= {
         :positions_covered => 0,
         :N => 0
       }
