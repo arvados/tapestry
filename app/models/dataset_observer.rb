c8fcdae186725cb8cf12e33c1d5f06ca233c62c6
Ward Vandewege <ward@clinicalfuture.com>
2012-11-05 10:25:05 -0500

    Also auto-assign PGP id to people who publish datasets anonymously.

--- app/models/dataset_observer.rb
+++ app/models/dataset_observer.rb
@@ -2,7 +2,7 @@ class DatasetObserver < ActiveRecord::Observer
 
   def assign_pgp_id(dataset)
     @u = dataset.participant
-    if not dataset.published_at.nil? and @u.pgp_id.nil? then
+    if (not dataset.published_at.nil? or not dataset.published_anonymously_at.nil?) and @u.pgp_id.nil? then
       @u.assign_pgp_id
     end
   end
