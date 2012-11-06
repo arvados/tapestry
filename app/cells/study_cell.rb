90ce424ff63092077eeb704b6e3fd86604ede19e
Tom Clegg <tom@clinicalfuture.com>
2012-11-06 08:10:14 -0800

    tweak third party projects list on dashboard. refs #922

--- app/cells/study_cell.rb
+++ app/cells/study_cell.rb
@@ -5,10 +5,14 @@ class StudyCell < Cell::Rails
 
   def dashboard_summary(options)
     @user = options[:user]
-    @studies_pending = Study.not_third_party.open_now.select do |s|
+    @studies_pending = Study.open_now.select do |s|
       sp = s.study_participants.where('user_id = ?', @user.id).first if @user
       sp.nil? or sp.is_undecided?
     end
+
+    @third_party_studies_pending = @studies_pending.select &:is_third_party
+    @studies_pending.reject! &:is_third_party
+
     render
   end
 
