3883a472be71583c138ad94d088d8e9d6b45aaa3
Tom Clegg <tom@clinicalfuture.com>
2012-12-07 23:45:28 -0800

    Collapse multi-file manifest into one table row + show/hide button. refs #1217

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -31,4 +31,49 @@ jQuery(document).ready(function($) {
 	    }
 	}
     });
+
+    if ($('tr[data-file-row]').length > 0) {
+	// Hide all files in multi-file manifests
+	$('tr[data-file-row][data-locator][data-index-in-manifest]').
+	    filter("[data-index-in-manifest!='']").
+	    hide();
+	// Make sure the first file in each manifest is showing
+	$('tr[data-file-row][data-locator][data-index-in-manifest]').each(function() {
+	    var locator = $(this).attr('data-locator');
+	    if (locator == "") return $(this).show();
+	    var trs = $('tr[data-locator="' + locator + '"]');
+	    trs.first().show();
+	    if (trs.length > 1) {
+		trs.first().attr('data-file-row-needs-showmore', trs.length);
+		// Make sure all rows from the same manifest are contiguous
+		trs.first().after(trs.filter(':gt(0)').detach());
+		// Remove the horizontal grid lines between files within a manifest
+		trs.filter(':gt(0)').children('td').css('border-top','none');
+	    }
+	});
+	// Add a "show more" button in the first column where needed
+	$('tr[data-file-row][data-file-row-needs-showmore]').each(function() {
+	    var colspan = $(this).children('td').length;
+	    var locator = $(this).attr('data-locator');
+	    var nfiles = $(this).attr('data-file-row-needs-showmore');
+	    $(this).
+		children('td').
+		first().
+		css('text-align','center').
+		append('<div style="width:16px;height:16px;padding:4px;margin:0;cursor:pointer" data-locator="' + locator + '" class="ui-widget ui-state-default ui-corner-all" title="Show all ' + nfiles + ' files in this collection"><span class="ui-icon ui-icon-plusthick"></span></div>(' + nfiles + ')');
+	});
+	// Toggle the non-first rows when "show more" is clicked
+	$('tr[data-file-row][data-locator] td:first').delegate('div.ui-widget', 'click', function() {
+	    var locator = $(this).attr('data-locator');
+	    var trs = $('tr[data-locator="' + locator + '"]');
+	    trs.filter('[data-index-in-manifest]:gt(0)').toggle();
+	    $(this).find('.ui-icon').
+		toggleClass('ui-icon-plusthick').
+		toggleClass('ui-icon-minusthick');
+	    // Remove cell bottom borders
+	    trs.children('td').css('border-bottom','1px solid #ccc');
+	    trs.filter(':visible').not(':last').children('td').css('border-bottom','none');
+	    return false;
+	});
+    }
 });
