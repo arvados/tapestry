9d668b80a5bf9b3c9933d33a966e859a582ca736
Tom Clegg <tom@clinicalfuture.com>
2013-01-08 13:03:07 -0800

    add /admin/mail_preview/dataset_notification

--- app/controllers/admin/user_mailer_previews_controller.rb
+++ app/controllers/admin/user_mailer_previews_controller.rb
@@ -2,8 +2,7 @@ class Admin::UserMailerPreviewsController < Admin::AdminControllerBase
 
   def email_change_notification
     @u = User.new(:email => 'new_email@example.com', :first_name => 'Axel', :last_name => 'Foley')
-    @text = UserMailer.email_change_notification(@u, 'old_email@example.com')
-    render :text => @text, :content_type => 'text/plain'
+    render_plain UserMailer.email_change_notification(@u, 'old_email@example.com')
   end
 
   def unclaimed_kit_reminder
@@ -14,6 +13,22 @@ class Admin::UserMailerPreviewsController < Admin::AdminControllerBase
     @sp ||= StudyParticipant.where('kit_last_sent_at is not ?', nil).last
     @sp ||= StudyParticipant.last
     @sp ||= StudyParticipant.new(:user => current_user, :study => Study.new)
-    render :text => UserMailer.unclaimed_kit_reminder(@sp).to_s, :content_type => 'text/plain'
+    render_plain UserMailer.unclaimed_kit_reminder(@sp)
+  end
+
+  def dataset_notification
+    @u = User.where('hex=?', params[:a]).first || current_user
+    render_plain UserMailer.dataset_notification_message specimen_analysis_data_index_url, @u
+  end
+
+  protected
+  def render_plain(t)
+    t = t.to_s
+    if t.match /: quoted-printable/
+      t.gsub!(/=\r\n/, "")
+      t.gsub!(/=([\dA-F]{2})/) { $1.hex.chr }
+    end
+    render(:text => t,
+           :content_type => 'text/plain')
   end
 end
