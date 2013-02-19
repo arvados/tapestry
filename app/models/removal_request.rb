484cec54a140f538b68091ae1fff980eb5d956d2
Ward Vandewege <ward@clinicalfuture.com>
2013-02-19 16:55:36 -0500

    Add a missing belongs_to on the RemovalRequest object.
    This closes #1379

--- app/models/removal_request.rb
+++ app/models/removal_request.rb
@@ -3,4 +3,6 @@ class RemovalRequest < ActiveRecord::Base
   stampable
   acts_as_paranoid_versioned :version_column => :lock_version
   belongs_to :user
+
+  belongs_to :fulfilled_by, :class_name => 'User', :foreign_key => 'fulfilled_by'
 end
