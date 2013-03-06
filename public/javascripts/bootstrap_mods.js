fa9c7aff86dd61f03889b8e888f4055412eedad3
Tom Clegg <tom@clinicalfuture.com>
2013-03-06 14:19:55 -0800

    add comments

--- public/javascripts/bootstrap_mods.js
+++ public/javascripts/bootstrap_mods.js
@@ -1,5 +1,12 @@
 jQuery(function(){
     var $ = jQuery;
+
+    // Change bootstrap dropdown menu behavior.  When a menu dropdown
+    // is active, hovering on a second menu in the same nav bar should
+    // cause the second menu to open.  (This is how menus work in
+    // desktop applications, so it is less surprising to users.  It
+    // also allows users to scan the menus quickly without a lot of
+    // clicking.)
     $('.dropdown-toggle').parent('.dropdown').on('hover', function(e) {
         if (!$(this).hasClass('open') &&
             $(this).parents('.nav').children('.dropdown.open').length > 0) {
