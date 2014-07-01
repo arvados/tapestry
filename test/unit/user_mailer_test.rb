238546c996a54eb19aea06b42cbde82aca84c9c8
Phil Hodgson <bitbucket@philhodgson.net>
2014-07-01 09:17:49 -0400

    Fixes UserMailerTest deprecations and errors (refs #2930) * different access to url_helpers * specifying host when sending email * removing reference to TMail - not the mailer we use (and anyway it came from vendor's generator)

--- test/unit/user_mailer_test.rb
+++ test/unit/user_mailer_test.rb
@@ -5,22 +5,18 @@ class UserMailerTest < ActiveSupport::TestCase
   FIXTURES_PATH = File.dirname(__FILE__) + '/../fixtures'
   CHARSET = "utf-8"
 
-  include ActionView::Helpers::UrlHelper
-  include ActionController::UrlWriter
+  include Rails.application.routes.url_helpers
 
   def setup
     ActionMailer::Base.delivery_method = :test
     ActionMailer::Base.perform_deliveries = true
     ActionMailer::Base.deliveries = []
-
-    @expected = TMail::Mail.new
-    @expected.set_content_type "text", "plain", { "charset" => CHARSET }
   end
 
   should "set the right edit_password_url in password_reset email" do
     user = Factory(:user)
-    email = UserMailer.create_password_reset(user)
-    assert email.body.include?(edit_password_url(:id => user.id, :key => user.crypted_password)), 
+    email = UserMailer.password_reset(user)
+    assert email.body.include?(edit_password_url(:host => ROOT_URL, :id => user.id, :key => user.crypted_password)),
       "email body should include password reset url in:\n#{email.body}"
   end
 
