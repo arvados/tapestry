a43d80de22f6f268b156fd69c8938190e054ebab
Phil Hodgson <bitbucket@philhodgson.net>
2014-07-02 18:58:41 -0400

    Rewrite oath_services controller tests to make them do something along the lines of what the existing tests intended (refs #2930)

--- test/functional/admin/oauth_services_controller_test.rb
+++ test/functional/admin/oauth_services_controller_test.rb
@@ -1,49 +1,54 @@
 require 'test_helper'
 
 class Admin::OauthServicesControllerTest < ActionController::TestCase
-  setup do
-    @admin_oauth_service = oauth_services(:one)
-  end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:admin_oauth_services)
-  end
+  logged_in_as_admin do
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create admin_oauth_service" do
-    assert_difference('OauthService.count') do
-      post :create, :admin_oauth_service => @admin_oauth_service.attributes
+    setup do
+      @admin_oauth_service = oauth_services(:one)
     end
 
-    assert_redirected_to admin_oauth_service_path(assigns(:admin_oauth_service))
-  end
-
-  test "should show admin_oauth_service" do
-    get :show, :id => @admin_oauth_service.to_param
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, :id => @admin_oauth_service.to_param
-    assert_response :success
-  end
-
-  test "should update admin_oauth_service" do
-    put :update, :id => @admin_oauth_service.to_param, :admin_oauth_service => @admin_oauth_service.attributes
-    assert_redirected_to admin_oauth_service_path(assigns(:admin_oauth_service))
-  end
-
-  test "should destroy admin_oauth_service" do
-    assert_difference('OauthService.count', -1) do
-      delete :destroy, :id => @admin_oauth_service.to_param
+    should "get index" do
+      get :index
+      assert_response :success
+      assert_not_nil assigns(:admin_oauth_services)
     end
 
-    assert_redirected_to admin_oauth_services_path
+    should "get new" do
+      get :new
+      assert_response :success
+    end
+
+    should "create admin_oauth_service" do
+      assert_difference('OauthService.count') do
+        post :create, :admin_oauth_service => @admin_oauth_service.attributes
+      end
+
+      assert_redirected_to admin_oauth_service_path(assigns(:admin_oauth_service))
+    end
+
+    should "show admin_oauth_service" do
+      get :show, :id => @admin_oauth_service.to_param
+      assert_response :success
+    end
+
+    should "get edit" do
+      get :edit, :id => @admin_oauth_service.to_param
+      assert_response :success
+    end
+
+    should "update admin_oauth_service" do
+      put :update, :id => @admin_oauth_service.to_param, :admin_oauth_service => @admin_oauth_service.attributes
+      assert_redirected_to admin_oauth_service_path(assigns(:admin_oauth_service))
+    end
+
+    should "destroy admin_oauth_service" do
+      assert_difference('OauthService.count', -1) do
+        delete :destroy, :id => @admin_oauth_service.to_param
+      end
+
+      assert_redirected_to admin_oauth_services_path
+    end
+
   end
 end
