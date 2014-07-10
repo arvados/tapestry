5352b979376cc0c49f455c161ceaf8cade5c2209
Phil Hodgson <bitbucket@philhodgson.net>
2014-07-10 18:42:55 -0400

    2930: Passing studies (collection events) controller tests
    (cherry-picked from 2930)

--- test/functional/studies_controller_test.rb
+++ test/functional/studies_controller_test.rb
@@ -1,49 +1,172 @@
 require 'test_helper'
 
 class StudiesControllerTest < ActionController::TestCase
-  setup do
-    @study = studies(:one)
-  end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:studies)
-  end
+  should_eventually 'test the other actions: verify_participant_id, map, index_third_party, claim, users, etc.'
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+  context "without a logged in user" do
 
-  test "should create study" do
-    assert_difference('Study.count') do
-      post :create, :study => @study.attributes
+    context "on GET to new" do
+      setup do
+        get :new
+      end
+
+      should 'redirect appropriately' do
+        assert_redirected_to login_path
+      end
     end
 
-    assert_redirected_to study_path(assigns(:study))
-  end
+    context "on POST to create" do
+      setup do
+        @count = Study.count
+        post :create
+      end
 
-  test "should show study" do
-    get :show, :id => @study.to_param
-    assert_response :success
-  end
+      should_not set_the_flash.to /successfully created/i
 
-  test "should get edit" do
-    get :edit, :id => @study.to_param
-    assert_response :success
-  end
+      should 'redirect appropriately' do
+        assert_redirected_to login_path
+      end
 
-  test "should update study" do
-    put :update, :id => @study.to_param, :study => @study.attributes
-    assert_redirected_to collection_event_path(assigns(:study))
-  end
-
-  test "should destroy study" do
-    assert_difference('Study.count', -1) do
-      delete :destroy, :id => @study.to_param
+      should 'not increase the study count' do
+        assert_equal @count, Study.count
+      end
     end
 
-    assert_redirected_to studies_path
+    context "on GET to show" do
+      setup do
+        study = Factory :study
+        get :show, :id => study.to_param
+      end
+
+      should 'redirect appropriately' do
+        assert_redirected_to login_path
+      end
+    end
+
+    context "on PUT to update" do
+      setup do
+        @study = Factory :study
+        put :update, :id => @study.to_param, :study => { :participant_description => 'Crazy new participant_description' }
+      end
+
+      should_not set_the_flash.to /successfully updated/i
+
+      should 'redirect appropriately' do
+        assert_redirected_to login_path
+      end
+
+      should 'not have updated the participant_description' do
+        assert_not_equal Study.find(@study.to_param)[:participant_description], 'Crazy new participant_description'
+      end
+    end
+
+    context "on DELETE to destroy" do
+      setup do
+        @study = Factory :study
+        @count = Study.count
+        delete :destroy, :id => @study.to_param
+      end
+
+      should 'still be able to find the study' do
+        assert Study.find(@study)
+      end
+
+      should 'leave the study count as is' do
+        assert_equal @count, Study.count
+      end
+
+      should 'redirect appropriately' do
+        assert_redirected_to login_path
+      end
+    end
+
   end
+
+  logged_in_researcher_context do
+
+    context "on GET to new" do
+      setup do
+        get :new
+      end
+
+      should respond_with :success
+      should render_template :new
+    end
+
+    context "on POST to create" do
+      setup do
+        @count = Study.count
+        post :create, :study => Factory.attributes_for(:study)
+      end
+
+      should set_the_flash.to /successfully created/i
+
+      should 'redirect appropriately' do
+        assert_redirected_to page_path(:researcher_tools)
+      end
+
+      should 'increase the study count' do
+        assert_equal @count+1, Study.count
+      end
+    end
+
+    context "on GET to show" do
+      setup do
+        study = Factory :study, :creator => @user
+        get :edit, :id => study.to_param
+      end
+
+      should respond_with :success
+      should render_template :edit
+    end
+
+    context "on PUT to update" do
+      setup do
+        @study = Factory :study, :creator => @user
+        put :update, :id => @study.to_param, :study => { :participant_description => 'Crazy new participant_description' }
+      end
+
+      should set_the_flash.to /successfully updated/i
+
+      should 'redirect appropriately' do
+        assert_redirected_to page_path(:researcher_tools)
+      end
+
+      should 'have updated the participant_description' do
+        assert_equal assigns[:study][:participant_description], 'Crazy new participant_description'
+        assert_equal Study.find(@study)[:participant_description], 'Crazy new participant_description'
+      end
+    end
+
+
+    context "on DELETE to destroy" do
+      setup do
+        @study = Factory :study, :creator => @user
+        @count = Study.count
+        delete :destroy, :id => @study.to_param
+      end
+
+      should 'not be able to find the study' do
+        assert_raise ActiveRecord::RecordNotFound do
+          Study.find(@study)
+        end
+      end
+
+      should 'reduce the study count' do
+        assert_equal @count-1, Study.count
+      end
+
+      should 'redirect appropriately' do
+        assert_redirected_to collection_events_path
+      end
+    end
+
+  end
+
+  logged_in_enrolled_user_context do
+
+
+  end
+
 end
