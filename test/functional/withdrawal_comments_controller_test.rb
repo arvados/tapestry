d432abfb2a6b6ebd475600c68d222ed33e9df5c9
Phil Hodgson <bitbucket@philhodgson.net>
2014-07-10 18:44:27 -0400

    Withdrawal comments functional tests passing (refs #2930)

--- test/functional/withdrawal_comments_controller_test.rb
+++ test/functional/withdrawal_comments_controller_test.rb
@@ -1,49 +1,120 @@
 require 'test_helper'
 
 class WithdrawalCommentsControllerTest < ActionController::TestCase
-  setup do
-    @withdrawal_comment = withdrawal_comments(:one)
-  end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:withdrawal_comments)
-  end
+  context "without a logged in user" do
+    context "on GET to index" do
+      setup do
+        get :index
+      end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create withdrawal_comment" do
-    assert_difference('WithdrawalComment.count') do
-      post :create, :withdrawal_comment => @withdrawal_comment.attributes
+      should 'redirect appropriately' do
+        assert_redirected_to login_path
+      end
     end
 
-    assert_redirected_to withdrawal_comment_path(assigns(:withdrawal_comment))
-  end
+    context "on GET to new" do
+      setup do
+        get :new
+      end
 
-  test "should show withdrawal_comment" do
-    get :show, :id => @withdrawal_comment.to_param
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, :id => @withdrawal_comment.to_param
-    assert_response :success
-  end
-
-  test "should update withdrawal_comment" do
-    put :update, :id => @withdrawal_comment.to_param, :withdrawal_comment => @withdrawal_comment.attributes
-    assert_redirected_to withdrawal_comment_path(assigns(:withdrawal_comment))
-  end
-
-  test "should destroy withdrawal_comment" do
-    assert_difference('WithdrawalComment.count', -1) do
-      delete :destroy, :id => @withdrawal_comment.to_param
+      should 'redirect appropriately' do
+        assert_redirected_to login_path
+      end
     end
 
-    assert_redirected_to withdrawal_comments_path
+    context "on POST to create" do
+      setup do
+        @count = WithdrawalComment.count
+        post :create
+      end
+
+      should_not set_the_flash.to /have been recorded/i
+
+      should 'redirect appropriately' do
+        assert_redirected_to login_path
+      end
+
+      should 'not increase the withdrawal_comment count' do
+        assert_equal @count, WithdrawalComment.count
+      end
+    end
+
+    context "on GET to show" do
+      setup do
+        withdrawal_comment = Factory :withdrawal_comment
+        get :show, :id => withdrawal_comment.to_param
+      end
+
+      should 'redirect appropriately' do
+        assert_redirected_to login_path
+      end
+    end
+
   end
+
+  logged_in_user_context do
+
+    context "on GET to index" do
+      setup do
+        get :index
+      end
+
+      should 'redirect appropriately' do
+        assert_redirected_to unauthorized_user_path
+      end
+    end
+
+    context "on GET to new" do
+      setup do
+        get :new
+      end
+
+      should respond_with :success
+      should render_template :new
+    end
+
+    context "on POST to create" do
+      setup do
+        @count = WithdrawalComment.count
+        post :create, :withdrawal_comment => Factory.attributes_for(:withdrawal_comment)
+      end
+
+      should set_the_flash.to /have been recorded/i
+
+      should 'redirect appropriately' do
+        assert_redirected_to withdrawal_comment_path(assigns(:withdrawal_comment))
+      end
+
+      should 'increase the withdrawal_comment count' do
+        assert_equal @count+1, WithdrawalComment.count
+      end
+    end
+
+    context "on GET to show" do
+      setup do
+        withdrawal_comment = Factory(:withdrawal_comment, :user => @user)
+        get :show, :id => withdrawal_comment.to_param
+      end
+
+      should respond_with :success
+      should render_template :show
+    end
+
+  end
+
+  logged_in_as_admin do
+    context "on GET to index" do
+      setup do
+        2.times do
+          Factory :withdrawal_comment, :user => Factory(:user)
+        end
+        get :index
+      end
+
+      should respond_with :success
+      should render_template :index
+    end
+  end
+
 end
