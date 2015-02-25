c9adde3a25a33ab2382fd0341045abf4bb35eb88
Phil Hodgson <bitbucket@philhodgson.net>
2015-02-25 09:42:12 +0100

    4980: remove link to open humans from third party activities index - note that this is the commit that will be reversed later, once open humans is fully ready for production; the only difference will be that we wil move the open humans listing in the index of studies to the _top_ of the list

--- test/functional/third_party_controller_test.rb
+++ test/functional/third_party_controller_test.rb
@@ -4,32 +4,10 @@ class ThirdPartyControllerTest < ActionController::TestCase
 
   logged_in_enrolled_user_context do
 
-    context 'open humans section enabled' do
-
-      setup do
-        ApplicationController.any_instance.stubs('include_section?').with(Section::OPEN_HUMANS).returns(true)
-      end
-
-      context 'but no existing open humans service' do
-        should 'not freak out' do
-          get :index
-          assert_response :success
-        end
-      end
-
-      context 'existing open humans service but no existing token' do
-
-        setup do
-          @oauth_service = Factory(:open_humans_oauth_service)
-        end
-
-        should 'display index' do
-          get :index
-          assert_response :success
-          assert_not_nil assigns(:open_humans_service)
-        end
-      end
-
+    should 'get the index' do
+      get :index
+      assert_response :success
     end
+
   end
 end
