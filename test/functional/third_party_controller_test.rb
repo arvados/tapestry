503a6930bddd78bded3977cae7ec56b70f95bde6
Ward Vandewege <ward@curoverse.com>
2015-11-16 13:49:14 -0500

    Revert "4980: remove link to open humans from third party activities index"
    This reverts commit c9adde3a25a33ab2382fd0341045abf4bb35eb88.
    
    closes #7675

--- test/functional/third_party_controller_test.rb
+++ test/functional/third_party_controller_test.rb
@@ -4,10 +4,32 @@ class ThirdPartyControllerTest < ActionController::TestCase
 
   logged_in_enrolled_user_context do
 
-    should 'get the index' do
-      get :index
-      assert_response :success
-    end
+    context 'open humans section enabled' do
 
+      setup do
+        ApplicationController.any_instance.stubs('include_section?').with(Section::OPEN_HUMANS).returns(true)
+      end
+
+      context 'but no existing open humans service' do
+        should 'not freak out' do
+          get :index
+          assert_response :success
+        end
+      end
+
+      context 'existing open humans service but no existing token' do
+
+        setup do
+          @oauth_service = Factory(:open_humans_oauth_service)
+        end
+
+        should 'display index' do
+          get :index
+          assert_response :success
+          assert_not_nil assigns(:open_humans_service)
+        end
+      end
+
+    end
   end
 end
