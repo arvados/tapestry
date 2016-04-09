cdb6a33042d77e594bf47e9946308122a1316463
Tom Clegg <tom@curoverse.com>
2016-04-09 11:45:44 -0400

    8699: Add "View report" link to public profile page.

--- test/functional/profiles_controller_test.rb
+++ test/functional/profiles_controller_test.rb
@@ -1,8 +1,27 @@
 require 'test_helper'
 
 class ProfilesControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  setup do
+    ApplicationController.any_instance.
+      stubs('include_section?').
+      with(Section::PUBLIC_PROFILE).
+      returns(true)
+
+    @user = Factory(:enrolled_user)
+    @user_file = FactoryGirl.create(:user_file,
+                                    :user => @user)
+    @dataset_report = FactoryGirl.create(:dataset_report,
+                                         :user_file => @user_file)
+    [@user, @user_file, @dataset_report].map(&:save!)
+  end
+
+  should "link to dataset report and download" do
+    get :public, :hex => @user.hex
+    assert_response :success
+    assert_select("a[href='/dataset_reports/#{@dataset_report.id}']" +
+                  ":content(View #{@dataset_report.title})")
+    assert_select("a[href='/user_file/download/#{@user_file.id}']" +
+                  "[rel=nofollow]" +
+                  ":content(Download)")
   end
 end
