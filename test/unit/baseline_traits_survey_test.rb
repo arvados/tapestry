a20d71268c835af7098805a1bac3294131f0f207
Ward Vandewege <ward@curoverse.com>
2017-04-23 23:36:40 -0400

    Add comment for removing deprecation warning in the future.
    refs #2930

--- test/unit/baseline_traits_survey_test.rb
+++ test/unit/baseline_traits_survey_test.rb
@@ -12,7 +12,8 @@ class BaselineTraitsSurveyTest < ActiveSupport::TestCase
   %w(citizen health_insurance health_or_medical_conditions prescriptions_in_last_year allergies).each do |boolean_attr|
     # PH: 2014-06-30 get a deprecation warning for this but can't replace with the following line because of "too many arguments" even though
     # I believe that allow_value has the (*values) signature...
-    # should allow_value(true, false).for(boolean_attr)
+    # WVW: update 2017-04-23: should allow_values works in newer versions of shoulda
+    #should allow_values(true, false).for(boolean_attr)
     should_allow_values_for(boolean_attr, true, false)
     should_not allow_value(nil).for(boolean_attr).with_message("can't be blank")
   end
