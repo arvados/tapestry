f7189971d6d748aef7896605974329cb228a9661
Ward Vandewege <ward@curoverse.com>
2017-04-26 11:32:01 -0400

    Jenkins doesn't have an interactive shell.
    refs #2930

--- build/jenkins.sh
+++ build/jenkins.sh
@@ -2,5 +2,5 @@
 
 docker build --tag=tapestry/test build/docker
 
-docker run -it -v $WORKSPACE:/home/tapestry/tapestry tapestry/test:latest  build/test/run-tapestry-tests.sh
+docker run -v $WORKSPACE:/home/tapestry/tapestry tapestry/test:latest  build/test/run-tapestry-tests.sh
 
