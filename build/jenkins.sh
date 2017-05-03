a38f62d7cc3f2ea80c013ea2eae6968079674048
Ward Vandewege <ward@curoverse.com>
2017-05-02 20:37:56 -0400

    Make the jenkins.sh script fail if WORKSPACE is not defined.
    No issue #

--- build/jenkins.sh
+++ build/jenkins.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [[ "$WORKSPACE" == "" ]]; then
+  echo
+  echo "The variable WORKSPACE needs to be defined."
+  echo
+  echo "Set it to the directory path of the tapestry source tree."
+  echo
+  exit 1
+fi
+
 docker build --tag=tapestry/test build/docker
 
 docker run -v $WORKSPACE:/home/tapestry/tapestry tapestry/test:latest  build/test/run-tapestry-tests.sh
